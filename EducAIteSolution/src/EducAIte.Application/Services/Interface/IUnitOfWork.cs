525d2eaaaed2c3c8ad1db63dc3309d65ccc69f7f
roginand <roginandv@gmail.com>
2026-03-16 11:21:20 +0800

    feat(unit-of-work): implement UnitOfWork pattern and register services in DI

---
+++ EducAIteSolution/src/EducAIte.Application/Services/Interface/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+namespace EducAIte.Application.Interfaces;
+
+using EducAIte.Domain.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    ICourseRepository Courses { get; }
+    IDocumentRepository Documents { get; }
+    IFlashcardRepository Flashcards { get; }
+    INoteRepository Notes { get; }
+    IStudentRepository Students { get; }
+    IStudentFlashcardRepository StudentFlashcards { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+}
