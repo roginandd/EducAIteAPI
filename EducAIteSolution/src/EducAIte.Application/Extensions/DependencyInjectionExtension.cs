062e69a51222485b3b2a5a9fb85809518cb4149f
jcatillo <jcatillo1121@gmail.com>
2026-03-16 14:35:37 +0800

    resolve DI registration after rebase/autostash

--- EducAIteSolution/src/EducAIte.Application/Extensions/DependencyInjectionExtension.cs
+++ EducAIteSolution/src/EducAIte.Application/Extensions/DependencyInjectionExtension.cs
@@ -8,41 +8,41 @@ using Mapster;
 
 namespace EducAIte.Application.Extensions;
 
-  public static class DependencyInjection
+public static class DependencyInjection
+{
+  public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
   {
-      public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
-      {
-        ConfigureMapster();
+    ConfigureMapster();
 
-        // Business Logic and Application Services
-        services.AddScoped<IAuthService, AuthService>();
-        services.AddScoped<ICourseService, CourseService>();
-        services.AddScoped<IDocumentService, DocumentService>();
-        services.AddScoped<IFlashcardService, FlashcardService>();
-        services.AddScoped<INoteService, NoteService>();
-        services.AddScoped<INoteOrderingService, NoteOrderingService>();
-        services.AddScoped<IResourceOwnershipService, ResourceOwnershipService>();
-        services.AddSingleton<ISqidService, SqidService>();
-        services.AddScoped<IStudentFlashcardService, StudentFlashcardService>();
-        services.AddScoped<IStudentService, StudentService>();
-        services.AddScoped<IStudyLoadService, StudyLoadService>();
+    // Business Logic and Application Services
+    services.AddScoped<IAuthService, AuthService>();
+    services.AddScoped<ICourseService, CourseService>();
+    services.AddScoped<IDocumentService, DocumentService>();
+    services.AddScoped<IFlashcardService, FlashcardService>();
+    services.AddScoped<INoteService, NoteService>();
+    services.AddScoped<INoteOrderingService, NoteOrderingService>();
+    services.AddScoped<IResourceOwnershipService, ResourceOwnershipService>();
+    services.AddSingleton<ISqidService, SqidService>();
+    services.AddScoped<IStudentFlashcardService, StudentFlashcardService>();
+    services.AddScoped<IStudentService, StudentService>();
+    services.AddScoped<IStudyLoadService, StudyLoadService>();
 
-        // Repositories
-        services.AddScoped<IStudentRepository, StudentRepository>();
-        services.AddScoped<ICourseRepository, CourseRepository>();
-        services.AddScoped<IDocumentRepository, DocumentRepository>();
-        services.AddScoped<IFlashcardRepository, FlashcardRepository>();
-        services.AddScoped<INoteRepository, NoteRepository>();
-        services.AddScoped<IStudentFlashcardRepository, StudentFlashcardRepository>();
-        services.AddScoped<IStudyLoadRepository, StudyLoadRepository>();
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    // Repositories
+    services.AddScoped<IStudentRepository, StudentRepository>();
+    services.AddScoped<ICourseRepository, CourseRepository>();
+    services.AddScoped<IDocumentRepository, DocumentRepository>();
+    services.AddScoped<IFlashcardRepository, FlashcardRepository>();
+    services.AddScoped<INoteRepository, NoteRepository>();
+    services.AddScoped<IStudentFlashcardRepository, StudentFlashcardRepository>();
+    services.AddScoped<IStudyLoadRepository, StudyLoadRepository>();
+    services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        return services;
-      }
-
-      private static void ConfigureMapster()
-      {
-          var config = TypeAdapterConfig.GlobalSettings;
-          config.Scan(typeof(DependencyInjection).Assembly); // scans EducAIte.Application
-      }
+    return services;
   }
+
+  private static void ConfigureMapster()
+  {
+    var config = TypeAdapterConfig.GlobalSettings;
+    config.Scan(typeof(DependencyInjection).Assembly); // scans EducAIte.Application
+  }
+}
